64628c570a15152f4d7e5ca5e257c3cc7c036d2e
VoltEdgeBuilds <voltedgebuilds@gmail.com>
2025-08-12 21:18:00 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
+// api/index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Set up your middleware
+app.use(bodyParser.json());
+
+// Import your API logic
+const chatRouter = require('./chat');
+app.use('/chat', chatRouter);
+
+// The key step: Export the Express app
+// Vercel will use this as your Serverless Function handler
+module.exports = app;
 
